6c8db11777c3bf1e5eca0e3b96a1bdb23dc3b1db
NAKATSUKA, Yukitaka <zool@zool.jpn.org>
2024-03-06 15:19:33 +0900

    Fix build-appimage.sh

--- build-appimage.sh
+++ build-appimage.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 rm -f ./opt/releases/*.AppImage ./opt/formula/*.rb
+if [ "x${VAGRANT_WSL_ENABLE_WINDOWS_ACCESS}" != "x" ]; then
+  unset VAGRANT_WSL_ENABLE_WINDOWS_ACCESS
+fi
 vagrant up --provision
 rsync -avz -e 'ssh -p 12022 -i ~/.vagrant.d/insecure_private_key -l vagrant' localhost:/vagrant/opt/ ./opt/
 vagrant halt
